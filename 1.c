519cfd74fc0cfcd63ac04be4a42cb9ec30da62d0
GarvSolanki70 <technoluckyo06@gmail.com>
2025-01-11 14:59:33 +0530

    git # Please enter the commit message for your changes. Lines starting

---
+++ 1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int arr[]={1,2,2,3,4,4,4,5,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+
+    int i=0,j=1;
+    while(j<=n-1){
+        if(arr[j] == arr[i]){
+            j++;
+        }
+        else{
+            i++;
+            arr[i]=arr[j];
+            j++;
+        }
+    }
+    
+    int a = i+1;
+
+    for(int i=0;i<a;i++){
+        printf("%d",arr[i]);
+    }
+
+    return 0;
+}
